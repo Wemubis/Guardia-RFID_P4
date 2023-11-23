4c9f6e6ca4f81778c5736fa2deef61b59050eabb
Fafnir <26090074+404fafnir@users.noreply.github.com>
2023-11-23 15:26:09 +0100

    debug

--- src/change_key_add_hash.ino
+++ src/change_key_add_hash.ino
@@ -11,14 +11,14 @@ void setup() {
 	Serial.begin(115200);
 	SPI.begin();
 	mfrc522.PCD_Init();
-	Serial.println(F("Scan the RFID card to change the keys..."));
+	Serial.println("Scan the RFID card to change the keys...");
   
 }
 
 void loop() {
   // CHECK IF NEW CARD
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    Serial.println(F("Card detected!"));
+    Serial.println("Card detected!");
 
     // GET UID
     String uid = "";
@@ -28,7 +28,7 @@ void loop() {
     }
     uid.toUpperCase();
     Serial.print(F("UID: "));
-    Serial.println(F(uid));
+    Serial.println(uid);
 
     // ADD HASH IN BLOCKS
     MFRC522::MIFARE_Key currentKey = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
@@ -44,7 +44,7 @@ void loop() {
       changeKey(&currentKey, &newKeyA, MFRC522::PICC_CMD_MF_AUTH_KEY_A, cur);
       cur += 2;
     }
-    Serial.println(F("Keys A changed successfully!"));
+    Serial.println("Keys A changed successfully!");
 
     cur = 3;
     // IN BLOCK 3 | 6 | 9
@@ -55,7 +55,7 @@ void loop() {
       changeKey(&currentKey, &newKeyB, MFRC522::PICC_CMD_MF_AUTH_KEY_B, cur);
       cur += 3;
     }
-    Serial.println(F("Keys B changed successfully!"));
+    Serial.println("Keys B changed successfully!");
 
     // Halt PICC (put the card into sleep mode | signal that the interaction is complete)
     mfrc522.PICC_HaltA();
@@ -63,13 +63,13 @@ void loop() {
     mfrc522.PCD_StopCrypto1();
 
     delay(1000);
-    Serial.println(F("Scan the RFID card to change the keys..."));
+    Serial.println("Scan the RFID card to change the keys...");
   }
 }
 
 void changeKey(MFRC522::MIFARE_Key *currentKey, MFRC522::MIFARE_Key *newKey, byte authKeyType, int cur) {
   for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
-    mfrc522.PCD_WriteRegister(MFRC522::SectorTrailBlock, i, newKey[i]);
+    mfrc522.PCD_WriteRegister(MFRC522::SectorTrailBlock, i, *newKey[i]);
     currentKey[i] = newKey[i];
 	}
   mfrc522.PCD_Authenticate(authKeyType, cur, currentKey, &(mfrc522.uid));
@@ -85,7 +85,7 @@ void add_hash(MFRC522::MIFARE_Key *key, char *hash) {
     status = mfrc522.MIFARE_Write(block, (byte *)&hash[i], 16);
     if (status != MFRC522::STATUS_OK) {
       Serial.print(F("MIFARE_Write() failed: "));
-      Serial.println(F(mfrc522.GetStatusCodeName(status)));
+      Serial.println(mfrc522.GetStatusCodeName(status));
       return;
     }
     i += 16;
