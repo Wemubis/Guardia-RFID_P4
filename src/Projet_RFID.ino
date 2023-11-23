4ed3cb20928f7c61167215a895ad1e24f5606aad
Wemubis <mewen.le.boudec@gmail.com>
2023-11-23 23:39:20 +0100

    modif but couldn't test with server - was not up

--- src/Projet_RFID.ino
+++ src/Projet_RFID.ino
@@ -4,26 +4,26 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-const char* ssid = "Seb";  // Nom du réseau WiFi
-const char* password = "12345678914";  // Mot de passe du réseau WiFi
-const char* serverUrl = "https://cloud.sebhost.fr/rfid-app/checkAccess.php";  // URL du serveur pour la vérification d'accès via RFID (utilisation de HTTPS)
+const char* ssid = "Seb";
+const char* password = "12345678914";
+const char* serverUrl = "https://cloud.sebhost.fr/rfid-app/checkAccess.php";  // URL du serveur pour la vérification d'accès via RFID
 
-#define SS_PIN 15  // Broche Slave Select pour le MFRC522 (D8)
-#define RST_PIN 2   // Broche Reset pour le MFRC522 (D4)
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Initialisation du MFRC522
+#define SS_PIN 15  // D8
+#define RST_PIN 2   // D4
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
-    Serial.begin(115200);  // Initialisation de la communication série
-    WiFi.begin(ssid, password);  // Connexion au réseau WiFi
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
-        Serial.println("Connecting to WiFi...");  // Attente de la connexion au WiFi
+        Serial.println("Connecting to WiFi...");
     }
     Serial.println("Connected to WiFi");
 
-    SPI.begin();  // Initialisation de la communication SPI
-    mfrc522.PCD_Init();  // Initialisation du MFRC522
+    SPI.begin();
+    mfrc522.PCD_Init();
 }
 
 void loop() {
@@ -31,58 +31,57 @@ void loop() {
 
     if (cardId != "") {
         WiFiClientSecure client;  // Utilisation de WiFiClientSecure pour HTTPS
-        client.setInsecure();  // Ignorer la vérification du certificat (non recommandé dans un environnement de production)
-
-        HTTPClient http;  // Initialisation de l'objet HTTPClient
+        client.setInsecure();  // Ignorer la vérification du certificat
+        HTTPClient http;
 
         String url = String(serverUrl) + "?cardId=" + cardId;  // Construction de l'URL avec l'identifiant de la carte
 
         if (http.begin(client, url)) {
-            int httpCode = http.GET();  // Requête HTTP GET
+            int httpCode = http.GET();
 
             if (httpCode == 200) {
                 DynamicJsonDocument doc(1024);  // Création d'un document JSON dynamique pour stocker la réponse
-                DeserializationError error = deserializeJson(doc, http.getString());  // Désérialisation de la réponse JSON
+                DeserializationError error = deserializeJson(doc, http.getString());
 
                 if (error) {
                     Serial.print("JSON parsing failed! Error: ");
-                    Serial.println(error.c_str());  // Affichage d'une erreur en cas d'échec de la désérialisation JSON
+                    Serial.println(error.c_str());
                 } else {
-                    const char* status = doc["status"];  // Extraction du statut de la réponse JSON
+                    const char* status_ = doc["status"];
 
-                    if (strcmp(status, "Autorisé") == 0) {
-                        Serial.println("Accès Autorisé");  // Affichage du message en cas d'accès autorisé
-                    } else if (strcmp(status, "Refusé") == 0) {
-                        Serial.println("Accès Refusé");  // Affichage du message en cas d'accès refusé
-                    } else if (strcmp(status, "Inconnu") == 0) {
-                        Serial.println("Carte Inconnue");  // Affichage du message en cas de carte inconnue
+                    if (strcmp(status_, "Autorisé") == 0) {
+                        Serial.println("Accès Autorisé");
+                    } else if (strcmp(status_, "Refusé") == 0) {
+                        Serial.println("Accès Refusé");
+                    } else if (strcmp(status_, "Inconnu") == 0) {
+                        Serial.println("Carte Inconnue");
                     } else {
-                        Serial.println("Statut Inconnu");  // Affichage du message en cas de statut inconnu
+                        Serial.println("Statut Inconnu");
                     }
                 }
             } else {
                 Serial.print("HTTP Code: ");
                 Serial.println(httpCode);
-                Serial.println("Error accessing server");  // Affichage d'une erreur en cas d'échec d'accès au serveur
+                Serial.println("Error accessing server");
             }
 
-            http.end();  // Libération des ressources de l'objet HTTPClient
+            http.end();
         }
     }
 
-    delay(1000);  // Attente d'une seconde avant la prochaine itération
+    delay(1000);
 }
 
 String readCard() {
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
         String cardId = "";
         for (byte i = 0; i < mfrc522.uid.size; i++) {
-            cardId += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");  // Formattage de l'octet en deux caractères hexadécimaux
+            cardId += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
             cardId += String(mfrc522.uid.uidByte[i], HEX);
         }
-        cardId.toUpperCase();  // Conversion de l'identifiant de la carte en majuscules
+        cardId.toUpperCase();
         return cardId;
     }
 
-    return "";  // Retourne une chaîne vide si aucune carte n'est détectée
+    return "";
 }
--- src/test_key_projet_C3.ino
+++ src/test_key_projet_C3.ino
@@ -3,152 +3,134 @@
 #include <ArduinoJson.h>
 #include <SPI.h>
 #include <MFRC522.h>
-
-//https://randomnerdtutorials.com/esp32-https-requests/#esp32-https-requests-httpclient-without-certificate
+#include <stdio.h>
+#include <string.h>
 
 const char* ssid = "Seb";
-const char* password = "12345678";
-const char* serverUrl = "https://172.20.10.2/rfid-app/checkAccess.php";
+const char* password = "12345678914";
+const char* serverUrl = "https://cloud.sebhost.fr/rfid-app/checkAccess.php";
 
-const char* rootCACertificate = \ 
-"-----BEGIN CERTIFICATE-----\n" \
-"MIID+zCCAuOgAwIBAgIUctKpCws5JnEwtzrPO75taz8y/rQwDQYJKoZIhvcNAQEL\n" \
-"BQAwgYwxCzAJBgNVBAYTAkZSMRMwEQYDVQQIDApTb21lLVN0YXRlMQ0wCwYDVQQH\n" \
-"DARMeW9uMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQxDDAKBgNV\n" \ 
-"BAMMA1NlYjEoMCYGCSqGSIb3DQEJARYZY2hlcmtpc2ViYXN0aWVuQGdtYWlsLmNv\n" \
-"bTAeFw0yMzExMjExNTIxNDNaFw0yNDExMjAxNTIxNDNaMIGMMQswCQYDVQQGEwJG\n" \
-"UjETMBEGA1UECAwKU29tZS1TdGF0ZTENMAsGA1UEBwwETHlvbjEhMB8GA1UECgwY\n" \
-"9w0BCQEWGWNoZXJraXNlYmFzdGllbkBnbWFpbC5jb20wggEiMA0GCSqGSIb3DQEB\n" \
-"SW50ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMQwwCgYDVQQDDANTZWIxKDAmBgkqhkiG\n" \ 
-"9w0BCQEWGWNoZXJraXNlYmFzdGllbkBnbWFpbC5jb20wggEiMA0GCSqGSIb3DQEB\n" \
-"AQUAA4IBDwAwggEKAoIBAQCG4w1JCzlMAmvByI49y1iRlBkuil4z5Hp0u4qZfW6p\n" \
-"uihpSfMuPH10YNdw5iAOHqagzz4MfZPSQ8kY4fBFfKGOGjna1Xg/buRNuvWItkk6\n" \
-"6TJ9gQzENnDdcNtLb8G18o0d4P9TCs/Bd7jp8VK4uNOLQxSOTFw7rFsiHOgqIfQK\n" \
-"pf2vGgtpqRoeUkzIVgMzryGxF2Z2PpUZKYuICi+S1PzFLTBW4l3gWt4EUd9/vL7Q\n" \
-"+TnQvkVQ2mLg0KOfjzULY1P1mcM4Vq2Epb+iY9J1hOXh+X8Ppxw//l1WGQi1tJ1A\n" \
-"WlFFbKzcGcYzr3sZYsjjla7rCqibPJ7SN2lmne/q9M7bAgMBAAGjUzBRMB0GA1Ud\n" \
-"DgQWBBQR8vSbptkn0SnhD/H/P/1/dXxNDzAfBgNVHSMEGDAWgBQR8vSbptkn0Snh\n" \
-"D/H/P/1/dXxNDzAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQB0\n" \
-"nJiny2TeH/R1svA+rZdbh9Z0tPXPlYKz4+PHChT0jAaAEft9xjUMXwdXdrJlcW7i\n" \
-"yuInZb7O10t2qZlatrC5BXDEPu+8YLiwXYMF2UD2gjJYAfeb4GLZ9E1vXDJAoCuI\n" \
-"MFnPbJ3uCV/s52GFTedjmptr6h8g8+JuPbKkNTmF4OPRI7jLG7oaN4XX3CZINQCl\n" \
-"F8mSXd/aycAWpJ0qNH+q95ad2dmuQkWpcLp/oBaQkuKl3QfvFCMdcpv0YcvV6ZxY\n" \
-"JG+3iDnA+M6i4vBdahtiKCmbqbmEa+KVKz6v9tKImEKf55j05CR04YmDePi3VyL1\n" \
-"lGuoePXq0N8RS6r1BGa+\n" \
-"-----END CERTIFICATE-----\n";
+#define SS_PIN D1
+#define RST_PIN D0
 
-#define SS_PIN D1 // D8
-#define RST_PIN D0 // D4
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+MFRC522::StatusCode status;
+
 // DEFINE NEW KEYS
-byte keyA[MFRC522::MF_KEY_SIZE] = {0x4F, 0x2E, 0x7A, 0x91, 0xC8, 0x3F};
-byte keyB[MFRC522::MF_KEY_SIZE] = {0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC};
-3
+MFRC522::MIFARE_Key  newKeyA = {0x4F, 0x2E, 0x7A, 0x91, 0xC8, 0x3F};
+// MFRC522::MIFARE_Key  newKeyB = {0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC};
+
+
 void setup() {
 	Serial.begin(115200);
 
 	WiFi.begin(ssid, password);
-	Serial.println(F("Connecting to WiFi..."));
+	Serial.println("Connecting to WiFi...");
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(1000);
-		Serial.print(F("."));
+		Serial.print(".");
 	}
-	Serial.println(F("Connected to WiFi!"));
+	Serial.println("Connected to WiFi!");
 
 	SPI.begin();
 	mfrc522.PCD_Init();
-	Serial.println(F("Scan RFID card to check for authorization..."));
+	Serial.println("Scan RFID card to check for authorization...");
 }
 
 void loop() {
-	// Look for new cards
-	if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-		Serial.println(F("Card detected!"));
+	String cardId = read_UID();
 
-		// Get UID
-		String uid = "";
-		for (byte i = 0; i < mfrc522.uid.size; i++) {
-			uid += String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-			uid += String(mfrc522.uid.uidByte[i], HEX);
-		}
-		uid.toUpperCase();
+	// Look for new cards
+	if (cardId != "") {
+		Serial.println("Card detected!");
 
 		// Authenticate with the modified keys
-		if (checkKeys(MFRC522::PICC_CMD_MF_AUTH_KEY_A, MFRC522::PICC_CMD_MF_AUTH_KEY_B)) {
+		if (checkKeysAndReadBlock() != MFRC522::STATUS_OK) {
 
 			// Effectuer une requête HTTPS au serveur
-			WiFiClientSecure *client = new WiFiClientSecure;
-			client->setCACert(rootCACertificate);
+	        WiFiClientSecure client;
+			client.setInsecure();  // Ignorer la vérification du certificat
+			HTTPClient http;
 
-			// WiFiClientSecure client;
-			HTTPClient https;
+			String url = String(serverUrl) + "?cardId=" + cardId;
 
-			String url = String(serverUrl) + "?cardId=" + uid;
-			https.begin(*client, url);
+			if (http.begin(*client, url)) {
+				int	httpCode = http.GET();
 
-			int httpsCode = https.GET();
+				if (httpCode == 200) {
+					// Analyser la réponse JSON
+					DynamicJsonDocument doc(1024);
+					DeserializationError error = deserializeJson(doc, http.getString());
 
-			if (httpsCode == 200) {
-				// Analyser la réponse JSON
-				DynamicJsonDocument doc(1024);
-				DeserializationError error = deserializeJson(doc, https.getString());
-
-				if (error) {
-					Serial.print(F("JSON parsing failed! Error: "));
-					Serial.println(F(error.c_str()));
-				} else {
-					// Vérifier le statut d'accès
-					const char* status = doc["status"];
-
-					if (strcmp(status, "Autorisé") == 0) {
-						Serial.println(F("Access Authorized"));
-					} else if (strcmp(status, "Refusé") == 0) {
-						Serial.println(F("Access Denied!"));
-					} else if (strcmp(status, "Inconnu") == 0) {
-						Serial.println(F("Card Unknown!"));
+					if (error) {
+						Serial.print("JSON parsing failed! Error: ");
+						Serial.println(error.c_str());
 					} else {
-						Serial.println(F("Unknown status"));
+						// Vérifier le statut d'accès
+						const char* status_ = doc["status"];
+
+						if (strcmp(status_, "Autorisé") == 0) {
+							Serial.println("Access Authorized");
+						} else if (strcmp(status_, "Refusé") == 0) {
+							Serial.println("Access Denied!");
+						} else if (strcmp(status_, "Inconnu") == 0) {
+							Serial.println("Card Unknown!");
+						} else {
+							Serial.println("Unknown status");
+						}
 					}
+				} else {
+					Serial.print("HTTP Code: ");
+					Serial.println(httpCode);
+					Serial.println("Error accessing server");
 				}
-			} else {
-				Serial.print(F("HTTP Code: "));
-				Serial.println(F(httpCode));
-				Serial.println(F("Error accessing server"));
+				https.end();
 			}
 
-			https.end();
-
-			mfrc522.PICC_HaltA();
-			mfrc522.PCD_StopCrypto1();
-
 		} else {
-			Serial.println(F("Authorization failed!"));
+			Serial.println("Authorization failed!");
 		}
 
 		delay(1000);
-
-		Serial.println(F("Scan RFID card to check for authorization..."));
+		Serial.println("Scan RFID card to check for authorization...");
 	}
 }
 
-int checkKeys(byte authKeyA, byte authKeyB) {
-	int	cur = 1;
+MFRC522::STATUS_OK checkKeysAndReadBlock() {
+	int		block = 4;
+	byte	lenBuffer = 18;
+	byte	readDataBlock[18];
 
-	while (cur < 6) {
-		if (!mfrc522.PCD_Authenticate(authKeyA, cur, keyA, &(mfrc522.uid)))
-			return (0);
-		cur += 2;
+	while (block < 64) {
+		status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &keyA, &(mfrc522.uid))
+		if (status != MFRC522::STATUS_OK) {
+			Serial.print("PCD_Authenticate() failed: ");
+			Serial.println(mfrc522.GetStatusCodeName(status));
+			return status;
+  		}
+		block += 4;
 	}
-	cur = 3;
-	while (cur < 13) {
-		if (!mfrc522.PCD_Authenticate(authKeyB, cur, keyB, &(mfrc522.uid)))
-			return (0);
-		cur += 3;
+
+	block = 1;
+	status = mfrc522.MIFARE_Read(block, readDataBlock, lenBuffer);
+	if (status != MFRC522::STATUS_OK) {
+		Serial.print("MIFARE_Write() failed: ");
+		Serial.println(mfrc522.GetStatusCodeName(status));
+		return status;
 	}
+	return status;
 }
 
-int checkKeys(byte authKeyA, byte authKeyB) {
-	
+String readCard() {
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+        String cardId = "";
+        for (byte i = 0; i < mfrc522.uid.size; i++) {
+            cardId += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+            cardId += String(mfrc522.uid.uidByte[i], HEX);
+        }
+        cardId.toUpperCase();
+        return cardId;
+    }
+    return "";
 }
-
